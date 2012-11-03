459fe0d4d053f0c32ec678060a670a9005ba164e
Ryan Lindeman <ryanlindeman@gmail.com>
2012-11-03 10:04:01 -0600

    CMake 2.8.2 compatibility fixes

--- cmake/Functions.cmake
+++ cmake/Functions.cmake
@@ -3,7 +3,14 @@
 # Modification Log:
 # 2012-01-28 Initial version
 # 2012-03-23 Added Actions.cmake script file
+# 2012-10-26 Special fix for cmake versions 2.8.0 to 2.8.3
 #
+
+# Create CMAKE_CURRENT_LIST_DIR if it doesn't exist (pre 2.8.4)
+if(NOT DEFINED CMAKE_CURRENT_LIST_DIR)
+  get_filename_component(CMAKE_CURRENT_LIST_DIR
+    ${CMAKE_CURRENT_LIST_FILE} PATH)
+endif(NOT DEFINED CMAKE_CURRENT_LIST_DIR)
 
 # Action script for performing functions during CMake parsing
 include(${CMAKE_CURRENT_LIST_DIR}/Actions.cmake)
